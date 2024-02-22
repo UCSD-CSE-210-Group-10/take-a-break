9960d3aa327e5b5b8a7f45a263aeda771179caba
Anmol Budhiraja <anmolbudhiraja1999@gmail.com>
2024-02-22 14:21:51 -0800

    Add Routing and EventCreationPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Login from './Login';
+// import Login from './Login';
+import CreateEvent from './CreateEvent';
 
 function App() {
   return (
     <div className="App" data-testid="app-head">
-      <Login/>
+      <CreateEvent/>
     </div>
   );
 }
