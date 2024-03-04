f5c96b292a5f56ef9b417b42e6f6927bcd10e361
Anmol Budhiraja <anmolbudhiraja1999@gmail.com>
2024-03-04 13:11:51 -0800

    Add FriendsUI

--- frontend/src/__tests__/Friends.test.jsx
+++ frontend/src/__tests__/Friends.test.jsx
@@ -4,15 +4,20 @@ import { MemoryRouter } from "react-router-dom";
 import FriendCard from '../FriendCard';
 
 test("Friend Card Renders Successfully", () => {
-    render(<MemoryRouter><FriendCard/></MemoryRouter>);
-    
-    const leftSec = screen.getByTestId('friend-card');
-    expect(leftSec).toBeInTheDocument();
+    const sampleFriend =  {
+        id: 1,
+        name: 'John Doe',
+        image: './UCSD-logo.png',
+    };
+
+    render(<MemoryRouter><FriendCard key={sampleFriend.id} friend={sampleFriend} /></MemoryRouter>);    
+    const friendCard = screen.getByTestId('friend-card');
+    expect(friendCard).toBeInTheDocument();
 })
 
 test("Friends Page Renders Successfully", () => {
     render(<MemoryRouter><Friends/></MemoryRouter>);
 
-    const leftSec = screen.getByTestId('friends-container');
-    expect(leftSec).toBeInTheDocument();
+    const friendContainer = screen.getByTestId('friends-container');
+    expect(friendContainer).toBeInTheDocument();
 })
