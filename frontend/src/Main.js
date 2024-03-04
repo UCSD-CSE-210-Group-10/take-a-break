d4d7b0936960f1f3a29601b14a6547054cdc89a3
Anmol Budhiraja <anmolbudhiraja1999@gmail.com>
2024-03-04 12:56:53 -0800

    Add Friends Path

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -5,7 +5,6 @@ import CreateEvent from "./CreateEvent";
 import Login from "./Login";
 import { Routes, Route } from "react-router-dom";
 import EventDetails from "./EventDetails";
-import EventDetailsSample from "./EventDetailsSample";
 import Friends from "./Friends";
 
 function Main() {
@@ -15,7 +14,7 @@ function Main() {
           <Route path="/" element={<Login />}></Route>
           <Route path="/events" element={<EventsPage />}></Route>
           <Route path="/events/:id" element={<EventDetails />}></Route>
-          <Route path="/events/sampleEvent" element={<EventDetailsSample />}></Route>
+          <Route path="/friends" element={<Friends />}></Route>
           <Route path="/admin/events/create" element={<CreateEvent />}></Route>
         </Routes>
     </div>
