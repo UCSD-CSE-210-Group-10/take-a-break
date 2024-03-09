96deb8c9d789588a548d3498733d360fe1a6c351
Gurnoor Singh Khurana <gurnoor6@gmail.com>
2024-03-09 12:44:54 -0800

    update frontend dockerfile

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+sudo docker network rm servernet
+sudo docker stop my-postgres-db react-frontend go-backend
+sudo docker rm my-postgres-db go-backend react-frontend
+sudo docker image rm -f postgres-image go-backend-image react-frontend-image
+
 sudo docker network create --subnet=172.18.0.0/16 servernet
 
 cd sql
@@ -10,3 +15,8 @@ cd ../backend
 sudo docker build -t go-backend-image .
 sudo docker run --net servernet -p 8080:8080 --ip 172.18.0.3 -d --name go-backend go-backend-image
 
+sleep 10
+
+cd ../frontend
+sudo docker build -t react-frontend-image .
+sudo docker run --net servernet -p 3000:3000 --ip 172.18.0.4 -d --name react-frontend react-frontend-image
