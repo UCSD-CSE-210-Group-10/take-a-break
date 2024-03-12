282a0401d96f4a9d1cbfb5fa4ea5f090d5190ad6
Anmol Budhiraja <anmolbudhiraja1999@gmail.com>
2024-03-12 13:01:28 -0700

    Revert Local testing change

--- backend/database/dbconnector.go
+++ backend/database/dbconnector.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -17,17 +18,11 @@ type DBConnection struct {
 func NewDBConnection() (*DBConnection, error) {
 	// Read environment variables
 
-	// host := os.Getenv("DB_HOST")
-	// port := os.Getenv("DB_PORT")
-	// user := os.Getenv("DB_USER")
-	// password := os.Getenv("DB_PASSWORD")
-	// dbname := os.Getenv("DB_NAME")
-
-	host := "localhost"
-	port := "5432"
-	dbname := "take-a-break"
-	user := "anmolbudhiraja"
-	password := "123456"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
 
 	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
